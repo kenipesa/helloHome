be1f23134122882bab4c6013419656be48405042
Nicholas Paro <nichparo@amazon.com>
2019-08-29 17:10:42 -0700

    initial commit

---
+++ src/main/java/com/kenipesa/helloHome/HelloHomeApplication.java
@@ -0,0 +1,13 @@
+package com.kenipesa.helloHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHomeApplication.class, args);
+	}
+
+}
