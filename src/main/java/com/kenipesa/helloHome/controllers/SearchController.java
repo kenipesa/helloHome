075c7dccb15f3e890aad7b73dec1f85cc383dd66
sapanapoudel <sapana.poudel17@outlook.com>
2019-09-03 10:32:47 -0700

    get and post route for search

--- src/main/java/com/kenipesa/helloHome/controllers/SearchController.java
+++ src/main/java/com/kenipesa/helloHome/controllers/SearchController.java
@@ -1,7 +1,34 @@
 package com.kenipesa.helloHome.controllers;
 
+import com.kenipesa.helloHome.models.ApplicationUser;
+import com.kenipesa.helloHome.models.SearchesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class SearchController {
+    @Autowired
+    ApplicationUser applicationUser;
+
+    @Autowired
+    SearchesRepository searchesRepository;
+
+    @GetMapping("/user/searches")
+    public String getAllSearches() {
+        return "searches";
+    }
+
+    @PostMapping("/user/searches")
+    public RedirectView makeSearch(String city, String state, Principal p, Model model ) {
+
+    }
+
+
+
 }
