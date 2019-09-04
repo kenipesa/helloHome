efc42bb81cc06f754503d95fde32e4aa3f5d5709
Nicholas Paro <nichparo@amazon.com>
2019-09-04 13:38:21 -0700

    fixed search result table to allow updates without duplicates

--- src/main/java/com/kenipesa/helloHome/models/SearchesRepository.java
+++ src/main/java/com/kenipesa/helloHome/models/SearchesRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SearchesRepository extends JpaRepository<Searches, Long> {
   public Searches findByBuyerId(Long applicationUserId);
+  public Searches findByCityAndState(String city, String state);
 }
