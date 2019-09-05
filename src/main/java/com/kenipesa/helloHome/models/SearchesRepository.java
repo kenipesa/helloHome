f4dcd5073e5daf41eb825abe643de3b10b282bff
Nicholas Paro <nichparo@amazon.com>
2019-09-05 14:10:49 -0700

    allow table to properly update for searches

--- src/main/java/com/kenipesa/helloHome/models/SearchesRepository.java
+++ src/main/java/com/kenipesa/helloHome/models/SearchesRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SearchesRepository extends JpaRepository<Searches, Long> {
   public Searches findByBuyerId(Long applicationUserId);
-  public Searches findByCityAndState(String city, String state);
+  public Searches findByCityAndStateAndBuyerId(String city, String state, Long buyerId);
 }
